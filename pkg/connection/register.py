47b3a1df695671b2e75f1b236f33117c2541615c
wjl0101 <71593523+wjl0101@users.noreply.github.com>
2021-12-07 22:44:33 +0800

    Update register.py

--- pkg/connection/register.py
+++ pkg/connection/register.py
@@ -138,6 +138,7 @@ class FudanConnection:
         print("📅今日日期是, ", today)
         
         if today == old_info["d"]["info"]["date"]:
+            print('debug--old_info["d"]["oldInfo"]["area"]',old_info["d"]["oldInfo"]["area"])
             self.send_wechat(old_info["d"]["oldInfo"]["area"])
             return True
         else:
